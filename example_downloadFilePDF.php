fbf3f68ab6ca78dcdba2ad27fe0d998d96320fe0
Manuel Häuser <m.haeuser@gedankengut.de>
2015-06-09 13:53:23 +0200

    Create example_downloadFilePDF.php

---
+++ example_downloadFilePDF.php
@@ -0,0 +1,33 @@
+<?php
+
+$strAPIKey = 'xxxxxxxxxxxxxxxxxxxxx';
+
+$strApiWsdlUrl = 'http://<domain>/gsales2/api/api.php?wsdl';
+
+ini_set("soap.wsdl_cache_enabled", "0");
+$client = new soapclient($strApiWsdlUrl); 
+
+// Rechnungen
+$arrResult = $client->getInvoicePDF($strAPIKey, 5);
+
+// Angebote
+#$arrResult = $client->getOfferPDF($strAPIKey, 21);
+
+// Gutschriften
+#$arrResult = $client->getRefundPDF($strAPIKey, 10);
+
+// Dokumente
+#$arrResult = $client->getDocumentFile($strAPIKey, 3);
+
+if (is_string($arrResult['result']->content)){
+	$arrFileNameParts = explode('/',$arrResult['result']->name);
+	header('Content-type: application/pdf');
+	header('Content-Disposition: attachment; filename="'.$arrFileNameParts[count($arrFileNameParts)-1].'"');
+	echo base64_decode($arrResult['result']->content);
+	return;
+}
+
+echo 'Ein Fehler ist aufgetreten';
+echo '<pre>';
+	print_r($arrResult);
+echo '</pre>';
